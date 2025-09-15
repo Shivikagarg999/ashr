cbbd35bfb3f53a40f8f106796fc49a99d9f2c146
Shivika garg <shivikagarg91@gmail.com>
2025-09-15 13:45:41 +0530

    Update Home.jsx jhv

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex-grow">
+      <main className="flex-grow pt-16 md:pt-20">
         
         {/* Pitr Paksh Donation Banner */}
         <section className="bg-gradient-to-r from-orange-600 via-orange-500 to-yellow-500 text-white py-4 px-6 md:px-12 relative overflow-hidden">
